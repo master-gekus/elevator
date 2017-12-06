bbaf039039fb28e41b61763f6ad7f1a8cc2e7cbf
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2017-12-06 07:33:31 +0300

    Implemented command line parsing

--- elevator/main.cpp
+++ elevator/main.cpp
@@ -1,9 +1,106 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <clocale>
+#include <cstring>
+#include <cinttypes>
 
-using namespace std;
+#define MIN_FLOORS_COUNT    5
+#define MAX_FLOORS_COUNT    20
+#define MIN_FLOOR_HEIGHT    2.0
+#define MAX_FLOOR_HEIGHT    10.0
+#define MIN_ELEVATOR_SPEED  0.1
+#define MAX_ELEVATOR_SPEED  10.0
+#define MIN_DOOR_OPEN_TIME  0.5
+#define MAX_DOOR_OPEN_TIME  120.0
 
-int main()
+namespace {
+    int floor_count;
+    int floor_timeout;
+    int door_timeout;
+
+    bool parse_command_line(int argc, char *argv[])
+    {
+        if (5 != argc) {
+            fprintf(stderr, "Command line error: Invalid argument count.\n");
+            return false;
+        }
+
+        floor_count = static_cast<int>(strtol(argv[1], nullptr, 0));
+        if ((MIN_FLOORS_COUNT > floor_count) || (MAX_FLOORS_COUNT < floor_count)) {
+            fprintf(stderr, "Command line error: Invalid number of floors.\n");
+            return false;
+        }
+
+        float floor_height = strtof(argv[2], nullptr);
+        if ((MIN_FLOOR_HEIGHT > floor_height) || (MAX_FLOOR_HEIGHT < floor_height)) {
+            fprintf(stderr, "Command line error: Invalid floor height.\n");
+            return false;
+        }
+
+        float elevator_speed = strtof(argv[3], nullptr);
+        if ((MIN_ELEVATOR_SPEED > elevator_speed) || (MAX_ELEVATOR_SPEED < elevator_speed)) {
+            fprintf(stderr, "Command line error: Invalid elevator speed.\n");
+            return false;
+        }
+
+        float door_open_time = strtof(argv[4], nullptr);
+        if ((MIN_DOOR_OPEN_TIME > door_open_time) || (MAX_DOOR_OPEN_TIME < door_open_time)) {
+            fprintf(stderr, "Command line error: Invalid door open time.\n");
+            return false;
+        }
+
+        floor_timeout = static_cast<int>((floor_height * 1000.0 / elevator_speed) + 0.5);
+        door_timeout = static_cast<int>((door_open_time * 1000.0) + 0.5);
+
+        return true;
+    }
+
+    void display_help(const char *argv)
+    {
+        const char *name = strrchr(argv,
+#ifdef _WIN32
+                                   '\\'
+#else
+                                   '/'
+#endif
+                                   );
+        if (nullptr == name) {
+            name = argv;
+        } else {
+            name++;
+        }
+        printf(
+            "Usage:\n"
+            "  %s <floors_count> <floor_height> <elevator_speed> <doors_open_time>\n\n"
+            "where:\n"
+            "  <floors_count>    - number of floors; integer from %d to %d.\n\n"
+            "  <floor_height>    - floor height in meters; decimal from %.1f to %.1f.\n\n"
+            "  <elevator_speed>  - elevator_speed in meters per secons; decimal from\n"
+            "                      %.1f to %.1f.\n\n"
+            "  <doors_open_time> - time between opening doors and closing them in\n"
+            "                      seconds; decimal from %.1f to %.1f.\n"
+        , name,
+        MIN_FLOORS_COUNT, MAX_FLOORS_COUNT,
+        MIN_FLOOR_HEIGHT, MAX_FLOOR_HEIGHT,
+        MIN_ELEVATOR_SPEED, MAX_ELEVATOR_SPEED,
+        MIN_DOOR_OPEN_TIME, MAX_DOOR_OPEN_TIME);
+    }
+}
+
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    setlocale(LC_ALL, "C");
+
+    if (!parse_command_line(argc, argv)) {
+        display_help(argv[0]);
+        return 1;
+    }
+
+    printf("Starting elevator with following parameters:\n"
+           "  Floors count      : %d\n"
+           "  Intrefloor timeout: %d ms\n"
+           "  Door close timeout: %d ms\n",
+           floor_count, floor_timeout, door_timeout);
+
     return 0;
 }
